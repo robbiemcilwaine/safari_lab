ed993c1649558e2f9414859306d73ee091aa51fa
Robbie Mcilwaine <robbiemcilwaine99@gmail.com>
2024-05-23 16:41:27 +0100

    finish MVP

--- safari.sql
+++ safari.sql
@@ -27,21 +27,11 @@ CREATE TABLE assignments (
 
 CREATE TABLE animals (
     id SERIAL,
-    name VARCHAR(255),
+    name VARCHAR(255) NOT NULL,
     type VARCHAR(255),
     age INT,
     enclosure_id INTEGER REFERENCES enclosure(id)
 );
-
-
-INSERT INTO animals (name, type, age) VALUES ('Poppy', 'Frog', 10, 2);
-INSERT INTO animals (name, type, age) VALUES ('Alex', 'Monkey', 10, 4);
-INSERT INTO animals (name, type, age) VALUES ('Leo', 'Lion', 8, 1);
-INSERT INTO animals (name, type, age) VALUES ('Bradley', 'Parrot', 20, 5);
-INSERT INTO animals (name, type, age) VALUES ('Jennifer', 'Camel', 30, 3);
-
-
-SELECT * FROM animals;
 
 
 INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Big Cat Field',30,FALSE);
@@ -50,7 +40,11 @@ INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Desert',
 INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Forest',45,TRUE);
 INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Canopy',29,FALSE);
 
-SELECT * FROM enclosure;
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Poppy', 'Frog', 10, 2);
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Alex', 'Monkey', 10, 4);
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Leo', 'Lion', 8, 1);
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Bradley', 'Parrot', 20, 5);
+INSERT INTO animals (name, type, age, enclosure_id) VALUES ('Jennifer', 'Camel', 30, 3);
 
 INSERT INTO staff (name, employee_number) VALUES ('Robbie', 108098);
 INSERT INTO staff (name, employee_number) VALUES ('Sabah', 809707);
@@ -58,36 +52,24 @@ INSERT INTO staff (name, employee_number) VALUES ('Rachel', 097098);
 INSERT INTO staff (name, employee_number) VALUES ('Sabrina', 098097);
 INSERT INTO staff (name, employee_number) VALUES ('George', 709780);
 
-SELECT * FROM staff;
-
 INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (1, 2, 'Wednesday');
 INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (2, 2, 'Monday');
 INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (3, 3, 'Tuesday');
 INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (4, 4, 'Friday');
 INSERT INTO assignments (employee_id, enclosure_id, day) VALUES (5, 5, 'Thursday');
 
-
-SELECT * FROM assignments;
-
-
 -- The names of the animals in a given enclosure
-
-SELECT * FROM animals 
-LEFT JOIN enclosure
-ON animals.enclosure_id = enclosure.id;
+SELECT animals.name FROM animals
+INNER JOIN enclosure
+ON animals.enclosure_id = enclosure.id
+WHERE enclosure.id = 1;
 
 -- The names of the staff working in a given enclosure
+SELECT staff.name FROM staff
+INNER JOIN assignments
+ON staff.id = assignments.employee_id
+INNER JOIN enclosure
+ON enclosure.id = assignments.enclosure_id
+WHERE enclosure.id = 2;
 
--- SELECT * FROM staff
-
--- find enclosure
--- find all staff associated with it
--- load 
-
-SELECT name FROM staff
-LEFT JOIN assignments
-ON enclosure_id = 2;
-
--- SELECT name FROM staff
--- INNER JOIN assignments
--- ON assignments.enclosure_id = enclosure.id
+-- 'ON' describes the shared attribute between two tables i.e. PK and FK
