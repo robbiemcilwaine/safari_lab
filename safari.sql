8fed73f8563eaae3859cf00d5e10a3b3feea7bc0
Sabah Yusuf <sabah.yusuf@outlook.com>
2024-05-23 14:53:31 +0100

    another commit

--- safari.sql
+++ safari.sql
@@ -10,7 +10,7 @@ CREATE TABLE enclosure(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     capacity INT,
-    closedForMaintaince BOOLEAN
+    closed_for_maintenance BOOLEAN
 
 );
 
@@ -35,17 +35,23 @@ CREATE TABLE animals (
     age INT,
     enclosure_id INTEGER REFERENCES enclosure(id)
 );
-INSERT INTO animals (name, type, age) VALUES ('Poppy', 'Dog', 10);
-INSERT INTO animals (name, type, age) VALUES ('Alex', 'Zebra', 10);
+INSERT INTO animals (name, type, age) VALUES ('Poppy', 'Frog', 10);
+INSERT INTO animals (name, type, age) VALUES ('Alex', 'Monkey', 10);
 INSERT INTO animals (name, type, age) VALUES ('Leo', 'Lion', 8);
-INSERT INTO animals (name, type, age) VALUES ('Bradley', 'Giraffe', 20);
-INSERT INTO animals (name, type, age) VALUES ('Jennifer', 'Hippo', 30);
+INSERT INTO animals (name, type, age) VALUES ('Bradley', 'Parrot', 20);
+INSERT INTO animals (name, type, age) VALUES ('Jennifer', 'Camel', 30);
 
 
 SELECT * FROM animals;
 
 
+INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Big Cat Field',30,FALSE);
+INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Frog Pond',100,FALSE);
+INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Desert',100,FALSE);
+INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Forest',45,TRUE);
+INSERT INTO enclosure (name, capacity, closed_for_maintenance) VALUES ('Canopy',29,FALSE);
 
 
 
 
+SELECT * FROM enclosure;
